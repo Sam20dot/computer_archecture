5d1a271408360b5ed69558ebf73c6126c35d27bc
sam20dot <nizaweaimesampray@gmail.com>
2026-07-06 08:09:34 +0200

    codes which are good for sending the assembly langauges

---
+++ cs:app/unit3/128_mult_generation.c
@@ -0,0 +1,13 @@
+#include <inttypes.h>
+typedef unsigned __int128 uint128_t; 
+
+
+
+void store_produ(uint128_t *dest, uint64_t x, uint64_t y) {
+
+*dest= x* (uint128_t )y;
+
+
+
+
+}
---
+++ cs:app/unit3/arthematic_logic_unit.c
@@ -0,0 +1,8 @@
+// then first we create the function which will be dealing with this first we are 
+long shift_left4_rightn(long x, long n)
+{
+x <<= 4;
+x >>= n;
+return x;
+}
+
