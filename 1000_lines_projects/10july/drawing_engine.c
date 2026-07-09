af4b41558cb7edbbc3c23a323394fc8e66e8ef98
sam20dot <nizaweaimesampray@gmail.com>
2026-07-10 01:02:57 +0200

    notebook codes like

---
+++ 1000_lines_projects/10july/drawing_engine.c
@@ -0,0 +1,46 @@
+// we are going to write the game engine which will draw the things on the screen 
+// here we will first drwa the line which will be printed on the screen to show the size of the screen as the screen as it 
+#include <stdio.h>
+
+#define MAX 133
+#define HEIGHT 40
+
+
+
+
+int main () {
+
+    for (int i=0; i<MAX; i++) {
+        // to draw the holizantal line 
+        printf ("_");                                                                                                          
+
+
+
+    }
+    printf ("\n");
+    // then the other line which will be vertical to be drwan 
+    for (int i=0; i<MAX ; i++) {
+        printf ("|");
+
+        for (int i=0;i<MAX;i++){
+
+
+        printf ("_");
+
+    }
+    printf ("|");
+
+    printf ("\n");
+    
+
+
+}
+}
+
+
+
+
+
+
+
+
