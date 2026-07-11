9aecbe93380db5815be6efa0fead995b68737858
sam20dot <nizaweaimesampray@gmail.com>
2026-07-11 08:10:54 +0200

    checking which sign to use for getting the least signficant bit value if is one is zero

---
+++ cod:hi/unit3/multpl_alogorithm.c
@@ -0,0 +1,19 @@
+// this file contains the multplications alorgorithm which  can be used to do
+#include <stdio.h>
+
+
+#define MAX_STEPS 32
+
+int main () {
+int multpicant=0b0011;
+int tester=0b0011;                            //this is here to help us to test if the last number is zero 
+int multpilcand=0b11000;
+int product;
+
+
+printf ("the and logic of test and multpicant :%#b\n",multpicant && tester);
+printf ("the and logic of test with multpicant:%#b\n",multpicant & tester);
+
+
+
+}
