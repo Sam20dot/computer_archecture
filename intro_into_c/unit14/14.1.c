308ebfa6be283a74a2021ab132c4f2cb6f200ae0
sam20dot <nizaweaimesampray@gmail.com>
2026-07-16 13:25:16 +0200

    infinity loop as with sleep function

---
+++ intro_into_c/unit14/14.1.c
@@ -0,0 +1,21 @@
+// we are going to create the event loop  which is infinit 
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+// the function 
+int main () {
+
+
+    while (1) {
+
+
+        printf ("\n the infinit loop has been started !!");
+        sleep(4);
+
+
+    }
+
+}
+
