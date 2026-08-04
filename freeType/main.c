fcb11511ba2640f78daeafc2806e0681cd061c60
sam20dot <nizaweaimesampray@gmail.com>
2026-08-04 11:24:58 +0200

    setting the character size and the its pixel size

--- freeType/main.c
+++ freeType/main.c
@@ -15,7 +15,40 @@ printf ("\n the number of the glyph found in the file are \n %ld \n",fontConte.f
 printf ("\n the number of font flags which can be used within the files are :%#lb\n",fontConte.face->face_flags);
 printf ("\n the adding the number of square of EM To track them in deep : \n %hu pixel per EMSquare(ppem )",fontConte.face->units_per_EM);
 // set the character size this was mean that at creation of handle or object which or instance of face 
-// all conditions 
+//setting the current positions 
+int error2= FT_Set_Char_Size (fontConte.face, 0, 16*64, 300,300);
+if (error2==FT_Err_Ok) {
+
+    printf ("\n the character size has been set successfully !!! \n");
+
+
+
+}
+else {
+
+    printf ("failed to initilzed ");
+
+
+
+}
+// setting the character size 
+int error4= FT_Set_Pixel_Sizes (
+        fontConte.face,
+        0,
+        16
+
+        );
+
+if (error4==FT_Err_Ok) {
+
+
+    printf("the pixel size has been set on the screen successfully!!!!\n");
+
+
+}
+else {
+
+    printf ("failed to loead the or to re size the character according to the pixel!! \n");
 
 
 }
