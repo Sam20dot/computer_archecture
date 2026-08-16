f8fc022c2e7ebdc26b6327e39ccdd9c2f62daf6e
sam20dot <nizaweaimesampray@gmail.com>
2026-08-16 11:48:27 +0200

    notice: in lock-free we need what a cpu can updata in one go so when we use somthings like array it does not works like is not lock free objects

---
+++ intro_into_c/unit16/tomic_funct.c
@@ -0,0 +1,20 @@
+// we are about to deal with the lock free and unlock free  functions 
+// the first function is 
+// atomic_is_lock_free(const volotile A *obj);
+#include <stdio.h>
+#include <stdatomic.h>
+
+
+int main () {
+
+    _Atomic int a;
+    _Atomic int b[1000];
+
+    printf ("\n enter the atomic is :%s \n",\
+            atomic_is_lock_free(&a) ?"look-free":"is not loock free");
+    printf ("\n the atomic values is :%s\n", atomic_is_lock_free(&b) ?"look-free":"is not lock free");
+
+
+
+
+}
